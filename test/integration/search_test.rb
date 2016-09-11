d2c57856c86f985453b892ee097c7290262a6218
Alexander Fedorenko <alexander@fedorenko.im>
2016-09-11 20:54:09 +0300

    Fixed Searches tests - now they actually testing things. Dummy things, but still.

--- test/integration/search_test.rb
+++ test/integration/search_test.rb
@@ -2,13 +2,14 @@ require 'test_helper'
 
 class SearchTest < ActionDispatch::IntegrationTest
   test 'should return new search id when keyword provided' do
-    get search_url('qwe')
+    get search_url('soprano')
 
-    assert_instance_of Integer, JSON.parse(response.body)
+    # TODO: Change Fixnum to Integer when Ruby 2.4 will be released
+    assert_instance_of Fixnum, JSON.parse(response.body)['id']
   end
 
   test 'should return error when no keyword provided' do
-    get search_url
+    get searches_url
 
     assert_response :bad_request
   end
