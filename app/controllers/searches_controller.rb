df14468e43a0fe49ae43e72e6be81653c32a4b38
Alexander Fedorenko <alexander@fedorenko.im>
2016-09-11 22:46:36 +0300

    Routing refactoring

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,8 @@
 class SearchesController < ApplicationController
   require 'open-uri'
 
-  def index
-    render json: {}, status: :bad_request
-  end
-
   def search
-    @search = Search.new(keyword: params[:id])
+    @search = Search.new(keyword: params[:keyword])
 
     if @search.save
 
@@ -27,7 +23,7 @@ class SearchesController < ApplicationController
     response = JSON.load(open('https://api.github.com/gists/d2cc9b6151105c0ddd7337a7a2dc1fda/comments'))
 
     response.each do |comment|
-      if JSON.parse(comment['body'])['keywords'].split(',').map(&:strip).include? params[:id]
+      if JSON.parse(comment['body'])['keywords'].split(',').map(&:strip).include? params[:keyword]
         SearchResult.create(comment: comment, search_id: @search.id)
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'gist-search/:id', to: 'searches#search', as: 'search'
-  get 'search_result/:id', to: 'searches#result', as: 'search_result'
+  get 'gist-search/:keyword', to: 'searches#search', as: 'search'
+  get 'gist-search-results/:id', to: 'searches#result', as: 'search_result'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
