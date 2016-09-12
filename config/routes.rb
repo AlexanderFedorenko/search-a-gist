ec3839a3ab2be726c992cacac173b51471a21337
Alexander Fedorenko <alexander@fedorenko.im>
2016-09-12 12:00:25 +0300

    Improve routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'gist-search/:keyword', to: 'searches#search', as: 'search'
+  get 'gist-search/:keyword', to: 'searches#search', as: 'search', id: /[a-zA-Z0-9]/
   get 'gist-search-results/:id', to: 'searches#result', as: 'search_result'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
